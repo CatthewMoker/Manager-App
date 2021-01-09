6d6b91679750413dc86151b4775cf349fcd58a9a
Matthew Coker <mattacoker@gmail.com>
2021-01-09 16:12:58 -0500

    added firebase and begun setup for actions and reducers

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: 'AIzaSyAHi7yK8TB9p-RMV-4kWaV3Qj4PYzvBAfg',
+        authDomain: 'manager-ab63c.firebaseapp.com',
+        projectId: 'manager-ab63c',
+        storageBucket: 'manager-ab63c.appspot.com',
+        messagingSenderId: '1076645605237',
+        appId: '1:1076645605237:web:d717cf1028c7c590c5ad3c',
+      });
+    } else {
+      firebase.app(); // if already initialized, use that one
+    }
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
