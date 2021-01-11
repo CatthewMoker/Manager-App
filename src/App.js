245865f419c99c21338a63832040421ae07a8cf0
Matthew Coker <mattacoker@gmail.com>
2021-01-11 17:09:30 -0500

    added employee list and employee create but the picker is experiencing some issue stil

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
-import reducers from './components/reducers';
+import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 import Router from './Router';
 
