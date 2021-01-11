9b6aa3f0295d5aa447d1b80c5d3ec0bccc2785d4
Matthew Coker <mattacoker@gmail.com>
2021-01-10 21:18:10 -0500

    removed keys from application

--- src/App.js
+++ src/App.js
@@ -6,16 +6,25 @@ import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from '@env';
+
 class App extends Component {
   componentWillMount() {
     if (!firebase.apps.length) {
       firebase.initializeApp({
-        apiKey: 'AIzaSyAHi7yK8TB9p-RMV-4kWaV3Qj4PYzvBAfg',
-        authDomain: 'manager-ab63c.firebaseapp.com',
-        projectId: 'manager-ab63c',
-        storageBucket: 'manager-ab63c.appspot.com',
-        messagingSenderId: '1076645605237',
-        appId: '1:1076645605237:web:d717cf1028c7c590c5ad3c',
+        apiKey: API_KEY,
+        authDomain: AUTH_DOMAIN,
+        projectId: PROJECT_ID,
+        storageBucket: STORAGE_BUCKET,
+        messagingSenderId: MESSAGING_SENDER_ID,
+        appId: APP_ID,
       });
     } else {
       firebase.app(); // if already initialized, use that one
